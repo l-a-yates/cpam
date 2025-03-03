b979e0c38250b33080600a5b638458194f4a2019
l-a-yates <luke.yates@utas.edu.au>
2025-03-04 09:04:56 +1100

    rm function shorthand

--- R/prepare_cpam.R
+++ R/prepare_cpam.R
@@ -448,7 +448,7 @@ process_count_matrix <- function(count_matrix, t2g, gene_level) {
       dplyr::left_join(t2g, by = "target_id") %>%
       dplyr::group_by(.data[["gene_id"]]) %>%
       dplyr::summarise(dplyr::across(-.data[["target_id"]], sum)) %>%
-      (\(x) {
+      (function(x) {
         as.matrix(x[, -1]) %>% `rownames<-`(dplyr::pull(x, "gene_id"))
       })
   } else {
--- cran-comments.md
+++ cran-comments.md
@@ -1,3 +1,8 @@
+## Resubmission
+This is a resubmission. In this version I have:
+
+* Removed use of function shorthand for compatibility with earlier R versions.
+
 ## R CMD check results
 
 0 errors | 0 warnings | 1 note
